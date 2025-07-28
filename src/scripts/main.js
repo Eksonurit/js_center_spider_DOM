ade570a7e9dcdd7f5e6156a322310320d0cbf1ab
Eksonurit <fizervitalij897@gmail.com>
2025-07-28 17:27:34 +0300

    fix problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const spiderElement = document.querySelector('.spider');
+const wallElement = document.querySelector('.wall');
 
-spiderElement.style.top = 50 + '%';
-spiderElement.style.left = 50 + '%';
-spiderElement.style.transform = 'translate(-50%, -50%)';
+const containerHeight = parseFloat(getComputedStyle(wallElement).height);
+const containerWidth = parseFloat(getComputedStyle(wallElement).width);
+
+const spiderHeight = parseFloat(getComputedStyle(spiderElement).height);
+const spiderWidth = parseFloat(getComputedStyle(spiderElement).width);
+
+spiderElement.style.top = (containerHeight - spiderHeight) / 2 + 'px';
+spiderElement.style.left = (containerWidth - spiderWidth) / 2 + 'px';
